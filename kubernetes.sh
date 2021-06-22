facf5469d9b9a8c57ec3f584db03c62e9ba509e2
Keith, Nguyen <keith.nguyen@sabre.com>
2021-06-21 22:30:56 -0500

    Fix

--- kubernetes.sh
+++ kubernetes.sh
@@ -3,18 +3,14 @@ complete -F __start_kubectl k
 alias k="kubectl"
 
 alias kc="k create"
-alias kcj="k create job"
 alias kcd="k create deployment"
 alias kci="k create ingress"
-alias kcn="k create namespace"
 alias kcr="k create role"
-alias kcs="k create service"
 
 alias ka="k apply -f"
 alias kd="k describe"
 alias kdd="k describe deployment"
 alias kdp="k describe pod"
-alias kds="k describe service"
 alias kdi="k describe ingress"
 
 alias kdel="k delete"
@@ -23,7 +19,6 @@ alias ke="k edit"
 alias ked="k edit deployment"
 alias kei="k edit ingress"
 alias kep="k edit pod"
-alias kes="k edit svc"
 
 alias kex="k explain"
 alias ker="k explain --recursive"
@@ -33,10 +28,9 @@ alias kgd="k get deploy"
 alias kgi="k get ingress"
 alias kgp="k get pods"
 alias kgn="k get nodes"
-alias kgs="k get svc"
-alias kge="k get events --sort-by=’.metadata.creationTimestamp’ |tail -8"
 
 alias kl="k logs"
+alias kt="k top"
 
 alias kr="k run"
 export nks="-n kube-system"
@@ -45,5 +39,4 @@ export ow="-o wide"
 export doy="--dry-run=client -o yaml"
 
 export ETCDCTL_API=3
-export k8s="https://k8s.io/examples"
 
